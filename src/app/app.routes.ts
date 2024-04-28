83f1bace0a3cae767e58c8d38b70b60fafd2649b
Jânio <janioofi@gmail.com>
2024-04-28 17:15:56 -0300

    Componente Usuario List Criado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,13 @@ import { NavComponent } from './components/nav/nav.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './auth/auth.guard';
 import { HomeComponent } from './components/home/home.component';
+import { UsuarioListComponent } from './components/usuario/usuario-list/usuario-list.component';
 
 export const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: '', component: NavComponent, canActivate: [AuthGuard] ,children: [
 
-    {path: 'home', component: HomeComponent}
-
+    {path: 'home', component: HomeComponent},
+    {path: 'usuario', component: UsuarioListComponent}
   ]}
 ]
---
+++ src/app/components/usuario/usuario-list/usuario-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usuario-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './usuario-list.component.html',
+  styleUrl: './usuario-list.component.css'
+})
+export class UsuarioListComponent {
+
+}
