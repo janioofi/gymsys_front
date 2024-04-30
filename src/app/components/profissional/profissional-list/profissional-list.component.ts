4f8ffb037f789905fb40610bc74334d39d1447c7
Jânio <janioofi@gmail.com>
2024-04-30 18:17:46 -0300

    componente profissional list criado

---
+++ src/app/components/profissional/profissional-list/profissional-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profissional-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './profissional-list.component.html',
+  styleUrl: './profissional-list.component.css'
+})
+export class ProfissionalListComponent {
+
+}
