47a91c0c1f69a75b76ba0af903f47a9d7c9f9336
Jânio <janioofi@gmail.com>
2024-05-01 17:26:12 -0300

    componente cliente-list criado

---
+++ src/app/components/cliente/cliente-list/cliente-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cliente-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './cliente-list.component.html',
+  styleUrl: './cliente-list.component.css'
+})
+export class ClienteListComponent {
+
+}
---
+++ src/app/models/cliente.ts
@@ -0,0 +1,11 @@
+export interface Cliente{
+  id_cliente?: any,
+  nome: string,
+  sobrenome: string,
+  apelido: string,
+  cpf: string,
+  email: string,
+  data_nascimento: any,
+  plano: string,
+  id_plano?: any
+}
